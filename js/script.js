77c75e6b2c332954a15e7097045645c47298f2a7
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-01-26 15:22:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Learner","Competitive Programmer","C++ Developer"],
+    strings:["Competitive Programmer","C++ Developer","Learner"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
