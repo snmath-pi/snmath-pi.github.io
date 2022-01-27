8c9a349a74f87295d18c69db5139a80b82b9d582
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-01-27 10:47:28 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* ============================== typing animation ============================ */
-var typed = new Typed(".typing",{
-    strings:["","Competitive Programmer","C++ Developer","Learner"],
-    typeSpeed:80,
-    BackSpeed:60,
-    loop:true
-})
