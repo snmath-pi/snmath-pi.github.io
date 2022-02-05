55dfe45f8d51c92d595d656c99c7768235f5780e
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-02-05 15:43:59 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -31,7 +31,7 @@ dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
-    document.body.classList.toggle("dark");
+    
 })
 window.addEventListener("load", () => {
     if(document.body.classList.contains("dark"))
