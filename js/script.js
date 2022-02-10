f9a0422757f6ffc9a801ab8c95f2e99c9a09ace4
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-02-10 20:48:30 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Learner","Competitive Programmer","C++ Developer"],
+    typeSpeed:70,
+    BackSpeed:70,
+    loop:true
+})
